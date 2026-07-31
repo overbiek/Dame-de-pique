678fc923bd533dcfa551665c26d87a0d851daf3a
overbiek <overbeekerobin@gmail.com>
2026-07-31 21:35:46 +0200

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ddp-v1';
+const CACHE = 'ddp-v2';
 const ASSETS = ['/', '/manifest.json', '/icon-192.png', '/icon-512.png', '/apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
